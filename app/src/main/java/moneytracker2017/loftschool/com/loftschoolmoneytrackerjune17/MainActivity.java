9f0f1939c48a8d0640a15069d822e99213f01799
Andrey Kisin <wolf_joker@inbox.ru>
2017-06-20 23:37:12 +0300

    First commit for make branche

--- app/src/main/java/moneytracker2017/loftschool/com/loftschoolmoneytrackerjune17/MainActivity.java
+++ app/src/main/java/moneytracker2017/loftschool/com/loftschoolmoneytrackerjune17/MainActivity.java
@@ -11,4 +11,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
 }
