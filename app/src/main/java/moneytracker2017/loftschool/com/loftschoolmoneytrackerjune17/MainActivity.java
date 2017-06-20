8933a71e556c9a51bb95b4a3dfb2a2ef1f6171c0
Andrey Kisin <wolf_joker@inbox.ru>
2017-06-20 23:13:42 +0300

    First commit for make branche

---
+++ app/src/main/java/moneytracker2017/loftschool/com/loftschoolmoneytrackerjune17/MainActivity.java
@@ -0,0 +1,14 @@
+package moneytracker2017.loftschool.com.loftschoolmoneytrackerjune17;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
